d8683843cc86d396020dd0677e0c62c527b545a1
Malu Abrami <marialuizaabrami@gmail.com>
2025-09-12 14:48:37 -0300

    feat - estilizando login page

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,34 +1,42 @@
 import Button from "react-bootstrap/Button";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import "./LoginPage.css";
 
 function Login() {
   return (
-    <div className="login-form">
-      <h1>Bem vindo de volta!</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+    <div className="login-page">
+      <div className="login-form">
+        <h1 className="login-title">Bem vindo de volta!</h1>
+        <Form>
+          <div className="form-container">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Email"
+                className="mb-3"
+              >
                 <Form.Control type="email" placeholder="name@example.com" />
                 <Form.Text className="text-muted">
-                    Nós nunca compartilhamos seu email com ninguém.
+                  Nós nunca compartilhamos seu email com ninguém.
                 </Form.Text>
-            </FloatingLabel>
-        </Form.Group>
+              </FloatingLabel>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-                <FloatingLabel controlId="floatingPassword" label="Senha">
-                    <Form.Control type="password" placeholder="Senha" />
-                </FloatingLabel>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <FloatingLabel controlId="floatingPassword" label="Senha">
+                <Form.Control type="password" placeholder="Senha" />
+              </FloatingLabel>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Lembre-se de mim" />
+              <Form.Check type="checkbox" label="Lembre-se de mim" />
             </Form.Group>
-        <Button variant="primary" type="submit">
-          Entrar
-        </Button>
-      </Form>
+            <Button variant="primary" type="submit">
+              Entrar
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
